788d4941044ef3349bd31f9ac6a2ce8831822442
Lhaíslla Cavalcanti <lhaislla@gmail.com>
2019-09-08 16:26:05 -0300

    Create PA_simples.py

---
+++ PA_simples.py
@@ -0,0 +1,11 @@
+#Progressão aritimetica com While
+
+a1 = int(input('Primeiro termo: '))
+r = int(input('razão: '))
+a10 = a1 + (10-1) *r
+while a10 !=
+
+
+for c in range(a1, a10 +r, r ):
+print('{}'.format(c),end= ' ')
+print('acabou')
